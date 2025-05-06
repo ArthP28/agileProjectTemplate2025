66c47fe1469dd2263382964520aef7a448a8acef
Megan Jensen <spottedeye2@gmail.com>
2025-05-06 13:23:36 -0500

    Working on initial main menu

--- code/main.cpp
+++ code/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;
 
+void printMainMenu();
+
 int main()
 {
     cout<<"Agile Project"<<endl;
+    
+    printMainMenu();
+    
     return 0;
+}
+
+void printMainMenu(){
+    string input;
+    cout<<"Please type number cooresponding to the option you want: "<<endl;
+    cout<<"1: Take attendance"<<endl;
+    cout<<"2: View attendance"<<endl;
+    
+    cin>>input;
+    
+    if(input == '1'){
+        
+    }
+    else if(input == '2'){
+        
+    }
+    else{
+        clrscr();
+        cout<<"Please enter a valid number!"<<endl;
+        printMainMenu();
+    }
 }
