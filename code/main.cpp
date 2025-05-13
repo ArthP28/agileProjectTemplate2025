b6df6d14727a80f2202a51092da0e2afef71cfae
BrandonCarls <bcarls@carthage.edu>
2025-05-13 14:01:53 -0500

    Added students

--- code/main.cpp
+++ code/main.cpp
@@ -5,8 +5,23 @@ using namespace std;
 int main()
 {
     AttendanceDBAbstraction db("StudentAttendance.sqlite");
-	db.InsertStudent("201204", "firt", "lad");
-	db.InsertSection("Mahoney Baloney", "49", "SMTWRF");
-	// db.InsertStudentEnroll("stu", "do", "49");
+	string in = "y";
+	while (in != "n") {
+		string id;
+		string first;
+		string last;
+		cout << "id\n";
+		cin >> id;
+		cout << "first\n";
+		cin >> first;
+		cout << "last\n";
+		cin >> last;
+		db.InsertStudent(id, first, last);
+		cout << "Continue?\n";
+		cin >> in;
+	}
+	// db.InsertStudent("201204", "firt", "lad");
+	// db.InsertSection("Mahoney Baloney", "49", "SMTWRF");
+/* 	db.InsertStudentEnroll("stu", "do", "49"); */
     return 0;
 }
