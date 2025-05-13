625ec767e962af208e8a48bcd1970380f186e1e3
Andy Del Real <afdelreal04@gmail.com>
2025-05-13 13:47:38 -0500

    added gitignore and fixed db path access

--- code/main.cpp
+++ code/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-    AttendanceDBAbstraction db("StudentAttendance.sqlite");
-	db.InsertStudent("201204", "firt", "lad");
-	db.InsertSection("Mahoney Baloney", "49", "SMTWRF");
-	// db.InsertStudentEnroll("stu", "do", "49");
+    AttendanceDBAbstraction db("./StudentAttendance.sqlite");
+	// db.InsertStudent("201204", "firt", "lad");
+	// db.InsertSection("Mahoney Baloney", "49", "SMTWRF");
+	db.InsertStudentEnroll("firt", "lad", "49");
     return 0;
 }
